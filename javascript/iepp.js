9cc7e4c78e1314f5f45a78c3b4182a07ae1b18cb
jonathantneal <jonathantneal@dbac2aea-0deb-cf35-ae31-d9cd4ccc73f5>
2010-10-26 22:16:15 +0000

    1.6.2

--- javascript/iepp.js
+++ javascript/iepp.js
@@ -1,71 +1,71 @@
-// iepp v1.6.1 MIT @jon_neal
-(function(win, doc){
-	var html5_elements = 'abbr|article|aside|audio|canvas|details|figcaption|figure|footer|header|hgroup|mark|meter|nav|output|progress|section|summary|time|video',
-		html5_elements_array = html5_elements.split('|'),
-		html5_elements_array_length = html5_elements_array.length,
-		html5_elements_replace = new RegExp('<(\/*)('+html5_elements+')', 'gi'),
-		html5_selector_match = new RegExp('(^|[^\\n]*?\\s)('+html5_elements+')([^\\n]*)({[\\n\\w\\W]*?})', 'gi'),
-		html5_selector_replace = new RegExp('(^|\\s)('+html5_elements+')', 'gi'),
-		doc_fragment = doc.createDocumentFragment(),
+// iepp v1.6.2 MIT @jon_neal
+(function(win, doc) {
+	var elems = 'abbr|article|aside|audio|canvas|details|figcaption|figure|footer|header|hgroup|mark|meter|nav|output|progress|section|summary|time|video',
+		elemsArr = elems.split('|'),
+		elemsArrLen = elemsArr.length,
+		elemRegExp = new RegExp('(^|\\s)('+elems+')', 'gi'), 
+		tagRegExp = new RegExp('<(\/*)('+elems+')', 'gi'),
+		ruleRegExp = new RegExp('(^|[^\\n]*?\\s)('+elems+')([^\\n]*)({[\\n\\w\\W]*?})', 'gi'),
+		docFrag = doc.createDocumentFragment(),
 		html = doc.documentElement,
 		head = html.firstChild,
-		style = doc.createElement('style'),
-		body = doc.createElement('body');
-	style.media = 'all';
-	function html5_shim(doc) {
-		var html5_elements_array_count = -1;
-		while (++html5_elements_array_count < html5_elements_array_length)
-			doc.createElement(html5_elements_array[html5_elements_array_count]);
+		bodyElem = doc.createElement('body'),
+		styleElem = doc.createElement('style'),
+		body;
+	function shim(doc) {
+		var a = -1;
+		while (++a < elemsArrLen)
+			doc.createElement(elemsArr[a]);
 	}
-	html5_shim(doc);
-	html5_shim(doc_fragment);
-	function parse_style_sheet_list(style_sheet_list, media) {
-		var style_sheet_list_length = style_sheet_list.length,
-			style_sheet_list_count = -1,
-			style_sheet_list_each,
-			css_text_array = [];
-		while (++style_sheet_list_count < style_sheet_list_length) {
-			style_sheet_list_each = style_sheet_list[style_sheet_list_count];
-			media = style_sheet_list_each.media || media;
-			css_text_array.push(parse_style_sheet_list(style_sheet_list_each.imports, media));
-			css_text_array.push(style_sheet_list_each.cssText);
+	function getCSS(styleSheetList, mediaType) {
+		var a = -1,
+			len = styleSheetList.length,
+			styleSheet,
+			cssTextArr = [];
+		while (++a < len) {
+			styleSheet = styleSheetList[a];
+			mediaType = styleSheet.media || mediaType;
+			cssTextArr.push(getCSS(styleSheet.imports, mediaType));
+			cssTextArr.push(styleSheet.cssText);
 		}
-		return css_text_array.join('');
+		return cssTextArr.join('');
 	}
+	shim(doc);
+	shim(docFrag);
+	head.insertBefore(styleElem, head.firstChild);
+	styleElem.media = 'all';
 	win.attachEvent(
 		'onbeforeprint',
 		function() {
-			var html5_elements_array_count = -1,
-				html5_selector_matches,
-				html5_selector_matches_array = [],
-				style_sheet_cssText;
-			while (++html5_elements_array_count < html5_elements_array_length) {
-				var html5_elements_nodeList = doc.getElementsByTagName(html5_elements_array[html5_elements_array_count]),
-					html5_elements_nodeList_length = html5_elements_nodeList.length,
-					html5_elements_nodeList_count = -1;
-				while (++html5_elements_nodeList_count < html5_elements_nodeList_length)
-					if (html5_elements_nodeList[html5_elements_nodeList_count].className.indexOf('iepp_') < 0) html5_elements_nodeList[html5_elements_nodeList_count].className += ' iepp_'+html5_elements_array[html5_elements_array_count];
+			var a = -1,
+				cssText = getCSS(doc.styleSheets, 'all'),
+				cssTextArr = [],
+				rule;
+			body = body || doc.body;
+			while ((rule = ruleRegExp.exec(cssText)) != null)
+				cssTextArr.push((rule[1]+rule[2]+rule[3]).replace(elemRegExp, '$1.iepp_$2')+rule[4]);
+			styleElem.styleSheet.cssText = cssTextArr.join('\n');
+			while (++a < elemsArrLen) {
+				var nodeList = doc.getElementsByTagName(elemsArr[a]),
+					nodeListLen = nodeList.length,
+					b = -1;
+				while (++b < nodeListLen)
+					if (nodeList[b].className.indexOf('iepp_') < 0)
+						nodeList[b].className += ' iepp_'+elemsArr[a];
 			}
-			style_sheet_cssText = parse_style_sheet_list(doc.styleSheets, 'all');
-			while ((html5_selector_matches = html5_selector_match.exec(style_sheet_cssText)) != null)
-				html5_selector_matches_array.push(
-					(html5_selector_matches[1]+html5_selector_matches[2]+html5_selector_matches[3]).replace(html5_selector_replace, '$1.iepp_$2')
-					+html5_selector_matches[4]
-				);
-			head.insertBefore(style, head.firstChild);
-			style.styleSheet.cssText = html5_selector_matches_array.join('\n');
-			doc_fragment.appendChild(doc.body);
-			html.appendChild(body);
-			body.innerHTML = doc_fragment.firstChild.innerHTML.replace(html5_elements_replace, '<$1bdo');
+			docFrag.appendChild(body);
+			html.appendChild(bodyElem);
+			bodyElem.innerHTML = body.innerHTML.replace(tagRegExp, '<$1bdo');
+			bodyElem.className = body.className;
 		}
 	);
 	win.attachEvent(
 		'onafterprint',
 		function() {
-			body.innerHTML = '';
-			html.removeChild(body);
-			head.removeChild(style);
-			html.appendChild(doc_fragment.firstChild);
+			bodyElem.innerHTML = '';
+			html.removeChild(bodyElem);
+			html.appendChild(body);
+			styleElem.styleSheet.cssText = '';
 		}
 	);
 })(this, document);
